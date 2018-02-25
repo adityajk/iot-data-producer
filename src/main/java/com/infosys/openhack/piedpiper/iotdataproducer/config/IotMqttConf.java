5707b966be282ba623ec1335d89ee298147f2f81
Aditya <aditya05khajuria@gmail.com>
2018-02-26 01:35:24 +0530

    Support MQTT

--- src/main/java/com/infosys/openhack/piedpiper/iotdataproducer/config/IotMqttConf.java
+++ src/main/java/com/infosys/openhack/piedpiper/iotdataproducer/config/IotMqttConf.java
@@ -5,20 +5,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.annotation.MessagingGateway;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.channel.DirectChannel;
-import org.springframework.integration.core.MessageProducer;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.dsl.Pollers;
 import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory;
 import org.springframework.integration.mqtt.core.MqttPahoClientFactory;
-import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
 import org.springframework.integration.mqtt.outbound.MqttPahoMessageHandler;
-import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
 import org.springframework.integration.stream.CharacterStreamReadingMessageSource;
-import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageHandler;
-import org.springframework.messaging.MessagingException;
 
 /**
  * Created by aditya.khajuria on 2/20/2018.
